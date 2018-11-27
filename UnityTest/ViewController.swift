a89a4a99bc9350322e0ddf866da3452a8d3c3a9c
Ivan Rep <ivan.rep2@gmail.com>
2018-11-27 11:26:49 +0100

    Initial commit

---
+++ UnityTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnityTest
+//
+//  Created by Ivan Rep on 27/11/2018.
+//  Copyright © 2018 Ivan Rep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
