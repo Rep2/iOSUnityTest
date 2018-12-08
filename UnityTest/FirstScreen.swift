401e04a892ec868416dd57d4715c6bfa02857406
Ivan Rep <ivan.rep2@gmail.com>
2018-12-08 17:09:52 +0100

    Entry view

---
+++ UnityTest/FirstScreen.swift
@@ -0,0 +1,41 @@
+import SnapKit
+import UIKit
+
+class FirstScreen: UIViewController {
+    lazy var dark: UIButton = {
+        let button = UIButton(frame: .zero)
+
+        button.backgroundColor = .blue
+
+        button.setTitleColor(.white, for: .normal)
+        button.setTitleColor(.white, for: .selected)
+
+        button.titleLabel?.font = .systemFont(ofSize: 17)
+
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+        view.addSubview(dark)
+
+        dark.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(30)
+            make.centerY.equalToSuperview()
+            make.height.equalTo(60)
+        }
+    }
+
+    @objc
+    func didTapButton() {
+
+    }
+}
--- UnityTest/ViewController.swift
+++ UnityTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
             appDelegate.startUnity()
 
